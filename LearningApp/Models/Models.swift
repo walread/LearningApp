829a0103442a0cb577ba3e3a73282366548d7035
walread <will.alread@gmail.com>
2023-01-12 07:51:35 -0500

    - Added assets - Added data files - Added models - Parsed json in ContentModel - Parsed style data in ContentModel

---
+++ LearningApp/Models/Models.swift
@@ -0,0 +1,51 @@
+//
+//  Models.swift
+//  LearningApp
+//
+//  Created by Will Alread on 1/12/23.
+//
+
+import Foundation
+
+struct Module: Decodable, Identifiable {
+    
+    var id: Int
+    var category: String
+    var content: Content
+    var test: Test
+}
+
+struct Content: Decodable, Identifiable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var lessons: [Lesson]
+}
+
+struct Lesson: Decodable, Identifiable {
+    
+    var id: Int
+    var title: String
+    var video: String
+    var duration: String
+    var explaination: String
+}
+
+struct Test: Decodable, Identifiable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var questions: [Question]
+}
+
+struct Question: Decodable, Identifiable {
+    
+    var id: Int
+    var content: String
+    var correntIndex: String
+    var answers: [String]
+}
